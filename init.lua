0406afb278d4ec8bcfc5b34fb2c1f7056b621e10
jameschee <litcoderr@gmail.com>
2025-02-07 15:44:21 +0900

    update

--- init.lua
+++ init.lua
@@ -118,8 +118,6 @@ map['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '150'}}
 map['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', '150'}}
 map['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '200'}}
 map['<C-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '200'}}
-map['<C-y>'] = {'scroll', {'-0.10', 'false', '100'}}
-map['<C-e>'] = {'scroll', { '0.10', 'false', '100'}}
 neoscroll.setup({ mappings = map })
 
 -- ====================
